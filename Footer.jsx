a134078467f685e396604a4f8da2127f5e95752b
HarithaMN <76700110+HarithaMN@users.noreply.github.com>
2021-06-28 11:26:35 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <footer>
+      <p>Copyright by ShapeAI @ {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+
+export default Info;
